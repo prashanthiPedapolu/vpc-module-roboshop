db40e3f465ca831da67a21c217e63ed4edf2945f
prashanthiPedapolu <pedapolu.prashanthi24@gmail.com>
2025-09-03 10:57:16 -0400

    roboshop vpc

---
+++ variable.tf
@@ -0,0 +1,77 @@
+variable "project" {
+    type = string
+}
+variable "environment" {
+    type = string
+}
+variable "vpc_tags" {
+    type = map(string)
+    default = {}
+
+}
+variable "igw_tags" {
+    type = map(string)
+    default = {}
+
+}
+
+variable "cidr_block" {
+    default = "10.0.0.0/16"
+}
+variable "public_subnet_cidrs" {
+    type = list(string)
+}
+variable "public_subnet_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "private_subnet_cidrs" {
+    type = list(string)
+}
+variable "private_subnet_tags" {
+    type = map(string)
+    default = {}
+
+}
+variable "database_subnet_cidrs" {
+    type = list(string)
+    
+
+}
+variable "database_subnet_tags" {
+    type = map(string)
+    default = {}
+}
+variable "eip_tags" {
+    type = map(string)
+    default = {}
+}
+variable "nat_gateway_tags" {
+    type = map(string)
+    default = {}
+}
+variable "aws_public_route_table" {
+    type = map(string)
+    default = {}
+}
+variable "aws_private_route_table" {
+    type = map(string)
+    default = {}
+    
+}
+variable "aws_database_route_table" {
+    type = map(string)
+    default = {}
+    
+
+} 
+variable "is_peering_required" {
+    default = false
+}
+variable "vpc_peering_tags" {
+    type = map(string)
+    default = {}
+}
+
+
