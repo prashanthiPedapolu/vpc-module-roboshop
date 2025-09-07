5e5ec181597da292c6d9242d5bacc62ca58b95ca
prashanthiPedapolu <pedapolu.prashanthi24@gmail.com>
2025-09-06 21:12:06 -0400

    terraform

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,9 @@ output "vpc_id" {
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
